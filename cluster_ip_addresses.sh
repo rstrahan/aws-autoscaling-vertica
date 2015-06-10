c5908e6d27fd8a3e023eae34a766bc3f01d63d1f
Bob Strahan <bob.strahan@hp.com>
2015-06-10 19:59:40 -0400

    Add replace node detection, and README updates

---
+++ cluster_ip_addresses.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Returns a list of public / private IP addresses for each node in the cluster
+
+. ./autoscaling_vars.sh
+
+instanceIds=$(aws --output=text ec2 describe-instances --filters Name=tag-key,Values=Name,Name=tag-value,Values=$autoscaling_group_name Name=instance-state-code,Values=16 --query "Reservations[*].Instances[*].InstanceId")
+
+for instanceId in $instanceIds
+do
+   privateIps=$(aws --output=text ec2 describe-instances --instance-id $instanceId --query "Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddresses[*].PrivateIpAddress")
+   publicIps=$(aws --output=text ec2 describe-instances --instance-id $instanceId --query "Reservations[*].Instances[*].NetworkInterfaces[*].PrivateIpAddresses[*].Association.PublicIp")
+   echo "$instanceId: PublicIP [$publicIps], PrivateIP [$privateIps]"
+done
+
---
+++ down_node_check.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Run from cron schedule - checks for lifecycle hook termination messages on the SQS queue, and initiates cluster scaledown
+
+. /home/dbadmin/.bashrc
+PATH=/usr/local/bin:/opt/vertica/bin:${PATH}
+autoscaleDir=/home/dbadmin/autoscale
+. $autoscaleDir/autoscaling_vars.sh
+time=$( date +"%Y-%m-%d %H:%M:%S")
+
+# in non terminal mode, redirect stdout and stderr to logfile
+if [ ! -t 0 ]; then exec >> $autoscaleDir/down_node_check.log 2>&1; fi
+echo down_node_check.sh: [`date`]
+
+myIp=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
+# check nodes table for DOWN nodes
+if [ ! -z "$replace_down_node_after" -a $replace_down_node_after -gt 0 ]; then
+   downNodes=$(vsql -qAt -c "SELECT node_address FROM nodes WHERE node_state='DOWN' AND node_down_since < now()-'$replace_down_node_after minutes'::INTERVAL")
+   [ -z "$downNodes" ] && exit 0  # nothing to do!
+   for downNode in $downNodes
+   do
+      echo "Node [$downNode] DOWN for more than $replace_down_node_after minutes."
+      downSince=$(vsql -qAt -c "SELECT node_down_since FROM nodes WHERE node_address='$downNode'")
+      # already detected?
+      isDetected=$(vsql -qAt -c "select count(*) from autoscale.downNodes WHERE node_address='$downNode' AND node_down_since='$downSince'")
+      if [ $isDetected -eq 0 ]; then
+         # not already detected.. initiate termination
+         instId=$(vsql -qAt -c "SELECT ec2_instanceid FROM autoscale.launches WHERE node_address='$downNode' OR replace_node_address='$downNode' ORDER BY start_time DESC LIMIT 1")
+         time=$(date +"%Y-%m-%d %H:%M:%S")
+         echo "$myIp|$time|$downSince|$instId|$downNode" | \
+            vsql -c "COPY autoscale.downNodes (detected_by_node, trigger_termination_time, node_down_since, ec2_instanceid, node_address) FROM STDIN"
+         echo "Terminating EC2 instanceId [$instId]"
+         aws ec2 terminate-instances --instance-ids $instId
+         if [ $? -eq 0 ]; then
+            status="AWS EC2 termination OK - instanceId [$instId] / IP [$downNode]. This will trigger an auto scale replacement."
+         else
+            status="Error Returned [$?]: Cmd> aws ec2 terminate-instances --instance-ids $instId"
+         fi
+         echo "Update status: $status"
+         vsql -c "UPDATE autoscale.downNodes SET status='$status' WHERE node_address='$downNode' AND node_down_since='$downSince'; COMMIT;"
+      else
+         detectedBy=$(vsql -qAt -c "select detected_by_node from autoscale.downNodes WHERE node_address='$downNode' AND node_down_since='$downSince'")
+         echo "Node [$downNode] already terminated by node [$detectedBy]"
+      fi
+   done
+fi
+
+echo Done! [`date`]
+exit 0
